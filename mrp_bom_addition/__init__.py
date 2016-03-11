9ddb968713926cb16e45967ea7a8e08508f4ae4f
ponkhi403 <ponkhi403@gmail.com>
2016-03-11 19:57:15 +0600

    Bom weight addition upload and employee loan mgt. icon change

---
+++ mrp_bom_addition/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2016 MD Tanzilul Hasan Khan (<http://www.tanzilkhan.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+import mrp_bom_addition
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ mrp_bom_addition/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 MD Tanzilul Hasan Khan (<http://www.tanzilkhan.com>)
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    'name': 'BOM Weight Percentage Addition',
+    'version': '8.0.1.0',
+    'author': 'Tanzil Khan',
+    'category': 'Manufacturing',
+    'sequence': 1,
+    'summary': 'Percentage Calculation for BoM Components',
+    'depends': ['mrp'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'data': ['mrp_bom_addition_view.xml'],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
